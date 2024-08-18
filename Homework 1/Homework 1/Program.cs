081e7cb949e073a8c2e56c27621038020f59fb33
Quliyevh23 <aliquliyev1290@gmail.com>
2024-08-18 22:49:21 +0300

    VisualStudio task

---
+++ Homework 1/Homework 1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//Task1 (Ededin menfi ve ya usbet oldugunu tapan algortim)
+
+
+//int a = -231;
+
+//if (a == 0)
+//{
+//    Console.WriteLine("0 ededi ne menfi ne de mmusbet ededdir");
+//}
+//else if (a > 0)
+//{
+//    Console.WriteLine("Eded musbetdir");
+//}
+//else
+//{
+//    Console.WriteLine("Eded Menfidir");
+//}
+
+
+
+//Task2 (Ededin 3 ve 7 ye bolunduyunu tapan algoritm)
+
+
+//int a = 21;
+
+//if (a % 3 == 0 && a % 7 == 0)
+//{
+//    Console.WriteLine("Bu eded 3 ve 7 - e bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bu eded 3 ve 7-e eyni anda bolunmur");
+//}
